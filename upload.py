50c660a21b8e68785dd83c38f288a283a49566b4
Luigi Saetta <lsaetta@MBP-di-Luigi.lan>
2019-03-16 09:39:12 +0100

    added file list

--- upload.py
+++ upload.py
@@ -2,6 +2,8 @@
 # Oracle SE Team Italy
 # with special thnks to SuperBrack
 #
+# 16/03/2019: modified to support multiple file uploading
+#
 import oci
 import os
 import io
@@ -15,10 +17,10 @@ from oci.object_storage import ObjectStorageClient
 # the key is the key (PEM) you have uploaded to your profile 
 #
 config = {
-    "user": "ocid1.user.oc1..XXXXX",
-    "key_file": "/Users/lsaetta/Progetti/xxxx/oci_api_key.pem",
-    "fingerprint": "YYYYY",
-    "tenancy": "ocid1.tenancy.oc1..ZZZZZZ",
+    "user": "ocid1.XXXXX",
+    "key_file": "/Users/lsaetta/Progetti/xxx/oci_api_key.pem",
+    "fingerprint": "75:YYYYY",
+    "tenancy": "ocid1.ZZZZ",
     "region": "eu-frankfurt-1"
 }
 
@@ -29,7 +31,8 @@ def controlla_params():
     n_params = len(sys.argv)
 
     if (n_params < (N_PARAMS + 1)):
-        print("Usage: upload.py bucket_name local_file_path file_name")
+        print("Usage: upload.py bucket_name local_file_path file_names")
+        # file_names could be a list separated by comma (eg: file1,file2)
 
         sys.exit(-1)
     else:
@@ -52,17 +55,23 @@ print("Validazione configurazione OK")
 
 bucket_name = sys.argv[1]
 FILE_PATH = sys.argv[2]
-FILE_NAME = sys.argv[3]
+FILE_NAMES = sys.argv[3]
+
+# parse file_names
+FILE_NAMES = sys.argv[3].split(",") 
 
 object_storage_client = ObjectStorageClient(config)
 
 # get the namespace
 namespace = object_storage_client.get_namespace().data
 
-print('Uploading file {} ...'.format(FILE_NAME))
-
-object_storage_client.put_object(namespace, bucket_name, FILE_NAME, io.open(os.path.join(Path(FILE_PATH), FILE_NAME),'rb'))
-
-print("Upload completato !")
 print("")
+
+for FILE_NAME in FILE_NAMES:
+    print('Uploading file {} ...'.format(FILE_NAME))
+
+    object_storage_client.put_object(namespace, bucket_name, FILE_NAME, io.open(os.path.join(Path(FILE_PATH), FILE_NAME),'rb'))
+
+    print("Upload completato !")
+    print("")
 
